784007240645cfc0f944128f7ee5ae90cdca4657
harisson.castro <harisson.castro@FLE207D24.alunos.sc.senac.br>
2021-11-08 11:28:34 -0300

    CommitTeste001

---
+++ src/br/com/projetoIntegrador/entidade/TesteWaifu.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projetoIntegrador.entidade;
+
+/**
+ *
+ * @author harisson.castro
+ */
+public class TesteWaifu {
+    private String eve;
+    private String kat;
+
+    public String getEve() {
+        return eve;
+    }
+
+    public void setEve(String eve) {
+        this.eve = eve;
+    }
+
+    public String getKat() {
+        return kat;
+    }
+
+    public void setKat(String kat) {
+        this.kat = kat;
+    }
+    
+    
+}
